51feef62e92f37b8acbf47875a0038764efe77a5
DIPESHPURI GOSWAMI <dipeshpuri9@gmail.com>
2019-02-15 12:57:46 -0500

    Environment Setup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+
+router.get('/project', function(req, res, next) {
+  res.render('project', { title: 'project' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'services' });
+});
 module.exports = router;
